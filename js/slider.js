dd1c15d529a0c5b9797942c67753a9f5d17adcb9
Киленников Роман Сергеевич <KilennikovRS@kamaz.ru>
2021-10-15 10:06:26 +0300

    add slider

---
+++ js/slider.js
@@ -0,0 +1,16 @@
+swiper = new Swiper('.swiper', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+});
